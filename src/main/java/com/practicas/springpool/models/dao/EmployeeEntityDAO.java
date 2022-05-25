b1a6ac031117f7c1125dd78c15ece4302857ae1a
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2022-05-25 19:17:21 +0200

    main a cero

--- src/main/java/com/practicas/springpool/models/dao/EmployeeEntityDAO.java
+++
@@ -1,7 +0,0 @@
-package com.practicas.springpool.models.dao;
-
-import com.practicas.springpool.models.entities.EmployeeEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeEntityDAO extends CrudRepository<EmployeeEntity, Integer> {
-}
